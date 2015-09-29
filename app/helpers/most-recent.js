f805d5b71b12f6e22de8d7ad7dabcc657c8cbb9b
Aileen Cacayorin <acacayorin@gmail.com>
2015-09-29 15:24:08 -0700

    recent listings to limit to three

--- app/helpers/most-recent.js
+++ app/helpers/most-recent.js
@@ -1,24 +1,7 @@
 import Ember from 'ember';
 
 export function mostRecent(sortedListings) {
-  // var string = "";
-  // for (var i=0; i<3; i++) {
-  //   string += "<li>" + sortedListings[i].name + "</li><br>";
-  //
-  // }
-  // return string;
 
-  var array = []
-  var title = []
-    for (var i=0; i<3; i++) {
-    array.push(sortedListings[i]);
-  };
-
-    for (var i=0; i<array.length; i++) {
-    title.push(array[1]);
-  };
-debugger;
-  return title;
 }
 
 export default Ember.Helper.helper(mostRecent);
